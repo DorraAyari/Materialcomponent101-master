0fed40a56225e8ad6559a9b003bd92ac8fe3267d
DorraAyari <doura.ayari@gmail.com>
2021-11-15 10:13:36 +0100

    ShopApp

--- Materialcomponent101-master/mdc_100_series/lib/supplemental/product_columns.dart
+++ Materialcomponent101-master/mdc_100_series/lib/supplemental/product_columns.dart
@@ -36,57 +36,33 @@ class TwoProductCardColumn extends StatelessWidget {
       double heightOfCards = (constraints.biggest.height - spacerHeight) / 2.0;
       double heightOfImages = heightOfCards - ProductCard.kTextBoxHeight;
       // TODO: Change imageAspectRatio calculation (104)
-      double imageAspectRatio = constraints.biggest.width / heightOfImages;
+ // TODO: Change imageAspectRatio calculation (104)
+      double imageAspectRatio = heightOfImages >= 0.0
+          ? constraints.biggest.width / heightOfImages
+          : 49.0 / 33.0;
 
       // TODO: Replace Column with a ListView (104)
       return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsetsDirectional.only(start: 28.0),
-            child: top != null
-                ? ProductCard(
-                    imageAspectRatio: imageAspectRatio,
-                    product: top!,
-                  )
-                : SizedBox(
-                    height: heightOfCards,
-                  ),
-          ),
-          const SizedBox(height: spacerHeight),
-          Padding(
-            padding: const EdgeInsetsDirectional.only(end: 28.0),
-            child: ProductCard(
-              imageAspectRatio: imageAspectRatio,
-              product: bottom,
-            ),
-          ),
-        ],
-      );
-    });
-  }
-}
+      }
 
 class OneProductCardColumn extends StatelessWidget {
-  const OneProductCardColumn({required this.product, Key? key})
-      : super(key: key);
+      const OneProductCardColumn({required this.product, Key? key}) : super(key: key);
 
   final Product product;
 
   @override
   Widget build(BuildContext context) {
     // TODO: Replace Column with a ListView (104)
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
+       return ListView(
+      physics: const ClampingScrollPhysics(),
+      reverse: true,
       children: <Widget>[
+                   const SizedBox(
+          height: 40.0,
+        ),
         ProductCard(
           product: product,
-        ),
-        const SizedBox(
-          height: 40.0,
         ),
       ],
     );
   }
-}
